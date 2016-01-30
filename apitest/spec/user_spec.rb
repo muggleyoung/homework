d471c8dd9104354cf88f9ca066e7524140897aaf
Yang Mei <muggleyoung@gmail.com>
2016-01-30 17:33:22 +0100

    extract building connection part to before

--- apitest/spec/user_spec.rb
+++ apitest/spec/user_spec.rb
@@ -5,21 +5,26 @@ require 'json-schema'
 
 describe 'users' do
 
-  it 'should return user information' do
-    conn = Faraday.new(:url => 'https://api.github.com', :headers => {'Accept' => 'application/vnd.github.v3+json'}) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  before(:each) do
+    connection = Faraday.new(:url => 'https://api.github.com', :headers => {'Accept' => 'application/vnd.github.v3+json'}) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
     end
 
-    response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
+    @response = connection.get do |req|
       req.url '/users/muggleyoung'
     end
+    @json = JSON.parse(@response.body)
+  end
 
-    json = JSON.parse(response.body)
-    expect(response).to be_success
+  it 'should return success when request for existing user' do
+    expect(@json['login']).to eql('muggleyoung')
+    expect(@response).to be_success
+  end
 
-    expect(JSON::Validator.validate('spec/user.json', json)).to be_truthy
+  it 'should return required user information' do
+    expect(JSON::Validator.validate('spec/user.json', @json)).to be_truthy
   end
 
 end
