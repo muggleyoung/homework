8b4ed8a8dc4f3f665745273ccddc3a17b8d3f8fe
Yang Mei <muggleyoung@gmail.com>
2016-01-30 12:45:49 +0100

    move page files into one folder

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,16 +1,13 @@
 Given(/^I visit GitHub$/) do
-  home = HomePage.new
-  home.visitGitHub
+  @home.visitGitHub
 end
 
 When(/^I log in with my credentials$/) do
-  login_page = LoginPage.new
-  login_page.visit
-  login_page.sign_in
+  @login_page.visit
+  @login_page.sign_in
 end
 
 Then(/^I should see welcome message on the page$/) do
-  dashboard = DashboardPage.new
-  dashboard.verify_user
+  @dashboard.verify_user
 end
 
