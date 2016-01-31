b1bce0ea736b3523df17aee05ed549ee0b5b5c30
Yang Mei <muggleyoung@gmail.com>
2016-01-31 13:40:59 +0100

    use faraday middleware for the json parser

--- apitest/spec/user_spec.rb
+++ apitest/spec/user_spec.rb
@@ -2,29 +2,24 @@ require 'faraday'
 require 'pry'
 require 'json'
 require 'json-schema'
+require 'faraday_middleware'
 
 describe 'users' do
-
-  before(:each) do
-    connection = Faraday.new(:url => 'https://api.github.com', :headers => {'Accept' => 'application/vnd.github.v3+json'}) do |faraday|
-      faraday.request  :url_encoded
-      faraday.response :logger
+  before do
+    @connection = Faraday.new(url: 'https://api.github.com', headers: {'Accept' => 'application/vnd.github.v3+json'}) do |faraday|
       faraday.adapter  Faraday.default_adapter
+      faraday.response :json, content_type: /\bjson$/
     end
-
-    @response = connection.get do |req|
-      req.url '/users/muggleyoung'
-    end
-    @json = JSON.parse(@response.body)
   end
 
   it 'should return success when request for existing user' do
-    expect(@json['login']).to eql('muggleyoung')
-    expect(@response).to be_success
+    response = @connection.get '/users/muggleyoung'
+    expect(response).to be_success
+    expect(response.body['login']).to eql('muggleyoung')
   end
 
   it 'should return required user information' do
-    expect(JSON::Validator.validate('spec/user.json', @json)).to be_truthy
+    response = @connection.get '/users/muggleyoung'
+    expect(JSON::Validator.validate('spec/schemas/user.json', response.body)).to be_truthy
   end
-
 end
